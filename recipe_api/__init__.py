3fd9e731e13e3e9258f865068442b347f0fad0ff
Florian Hauser <f.hauser@mail.de>
2023-01-23 10:08:40 +0100

    app factory pattern

--- recipe_api/__init__.py
+++ recipe_api/__init__.py
@@ -1,31 +1,25 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-def create_app(test_config=None):
-    db = SQLAlchemy()
+def create_app(config_class=Config):
+    # Create App and Config
     app = Flask(__name__, instance_relative_config=True)
-    
-    app.config.from_mapping(SECRET_KEY="toBeChanged")
-    app.config.from_mapping(SQLALCHEMY_DATABASE_URI="sqlite:///memory")
+    app.config.from_object(config_class)
 
-
-    if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
-
-    app.config.from_mapping(test_config)
-
+    # Register App to extensions
     db.init_app(app)
+    migrate.init_app(app)
 
-       # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # Register Blueprints
+    # TODO
 
-    @app.route("/api/1/healthy")
-    def hello():
-        return {"status": "The app runs!"}
 
     return app
+
+from recipe_api import models
---
+++ recipe_server.py
@@ -0,0 +1,3 @@
+from recipe_api import create_app
+
+app = create_app()
