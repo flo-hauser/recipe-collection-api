a7d489a30c86f3ed6c59f5d248fa2ce469e554f9
Florian Hauser <f.hauser@mail.de>
2023-01-23 10:08:40 +0100

    db access via sqlalchemy

--- recipe_api/__init__.py
+++ recipe_api/__init__.py
@@ -1,16 +1,22 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(test_config=None):
+    db = SQLAlchemy()
     app = Flask(__name__, instance_relative_config=True)
+    
     app.config.from_mapping(SECRET_KEY="toBeChanged")
+    app.config.from_mapping(SQLALCHEMY_DATABASE_URI="sqlite:///memory")
+
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
-        DATABASE=os.path.join(app.instance_path, 'recipe_api.sqlite'),
 
     app.config.from_mapping(test_config)
+
+    db.init_app(app)
 
        # ensure the instance folder exists
     try:
@@ -18,8 +24,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/api/1/healthy')
+    @app.route("/api/1/healthy")
     def hello():
-        return 'The app runs!'
+        return {"status": "The app runs!"}
 
     return app
