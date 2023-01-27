7becd5dd4a9ff15f03cf494a3ccbc381b8ed4924
Florian Hauser <f.hauser@mail.de>
2023-01-27 14:32:16 +0100

    test edit users

--- tests/test_api_user.py
+++ tests/test_api_user.py
@@ -69,6 +69,7 @@ def test_get_user(client, auth):
     res_data = response.json
     assert res_data["username"] == auth.user.username
     assert res_data["id"] == auth.user.id
+    assert res_data["email"] == auth.user.email
     assert "roles" in res_data
     assert "self" in res_data["_links"]
 
@@ -122,3 +123,113 @@ def test_get_user_with_invaid_id(client, auth):
     )
 
     assert response.status_code == 404
+
+
+def test_edit_user(client, auth):
+    auth.login()
+    user_id = auth.user.id
+    request_body = {"username": "foo", "email": "foo@abc.de", "password": "foo"}
+    response = client.put(
+        "/api/1/users/{:d}".format(user_id),
+        headers=auth.token_auth_header,
+        json=request_body,
+    )
+
+    assert response.status_code == 200
+    assert response.json["username"] == request_body["username"]
+    assert response.json["email"] == request_body["email"]
+    assert "password" not in response.json
+
+
+def test_edit_user_as_another_user(client, auth):
+    auth.login()
+    user_id = auth.user.id
+    request_body = {"username": "foo", "email": "foo@abc.de", "password": "foo"}
+    response = client.put(
+        "/api/1/users/{:d}".format(user_id + 1),
+        headers=auth.token_auth_header,
+        json=request_body,
+    )
+
+    assert response.status_code == 403
+
+
+def test_edit_user_as_admin(client, auth):
+    auth.login(username="admin", password="admin")
+    user_id = auth.user.id
+    request_body = {"username": "foo", "email": "foo@abc.de", "password": "foo"}
+    response = client.put(
+        "/api/1/users/{:d}".format(user_id + 1),
+        headers=auth.token_auth_header,
+        json=request_body,
+    )
+
+    assert response.status_code == 200
+
+
+def test_edit_user_with_malformed_body(client, auth):
+    auth.login()
+    user_id = auth.user.id
+    request_bodies = [
+        {"username": "foo", "email": "foo@abc.de"},
+        {"username": "foo", "password": "foo"},
+        {"email": "foo@abc.de", "password": "foo"},
+    ]
+
+    for rb in request_bodies:
+        response = client.put(
+            "/api/1/users/{:d}".format(user_id),
+            headers=auth.token_auth_header,
+            json=rb,
+        )
+        assert response.status_code == 400
+
+
+def test_edit_user_with_existing_name_email(client, auth):
+    auth.login()
+    user_id = auth.user.id
+    request_bodies = [
+        {"username": "user_2", "email": "foo@abc.de", "password": "foo"},
+        {"username": "foo", "email": "user_2@example.com", "password": "foo"},
+    ]
+
+    for rb in request_bodies:
+        response = client.put(
+            "/api/1/users/{:d}".format(user_id),
+            headers=auth.token_auth_header,
+            json=rb,
+        )
+        assert response.status_code == 400
+
+
+def test_get_users(client, auth):
+    auth.login(username="admin", password="admin")
+    response = client.get(
+        "/api/1/users",
+        headers=auth.token_auth_header,
+    )
+
+    assert response.status_code == 200
+    assert type(response.json) == list
+    assert len(response.json) > 1
+    assert "username" in response.json[0]
+    assert "email" in response.json[0]
+    assert "_links" in response.json[0]
+    assert "id" in response.json[0]
+    assert "roles" in response.json[0]
+
+
+def test_get_users_as_non_admin(client, auth):
+    auth.login()
+    response_1 = client.get(
+        "/api/1/users",
+        headers=auth.token_auth_header,
+    )
+    auth.logout()
+    response_2 = client.get(
+        "/api/1/users",
+        headers=auth.token_auth_header,
+    )
+
+    assert response_1.status_code == 403
+    assert response_2.status_code == 401
