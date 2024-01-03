579f82b4471a8d2952b133b2c795fb0993820d1f
Florian Hauser <f.hauser@mail.de>
2024-01-03 10:27:00 +0100

    fix: image dev server

--- app/image_server/images.py
+++ app/image_server/images.py
@@ -9,21 +9,8 @@ import os
 
 
 @bp.route("/images/<int:recipe_id>/<string:image_file>", methods=["GET"])
-@token_auth.login_required
 def get_image(recipe_id, image_file):
-    # User owns requested recipe
-    user: User = token_auth.current_user()
-    result = db.session.execute(
-        db.select(Recipe)
-        .join(User.recipes)
-        .where(Recipe.id == recipe_id)
-        .where(User.id == user.id)
-    )
-    recipe = result.scalars().one_or_none()
-    if not recipe:
-        abort(404)
-
-    # validate image_file
+    # validate image_file path
     expr = re.compile(r"[a-z0-9]{32}.[a-z]{3,4}(.thumbnail)?")
     if not expr.match(image_file):
         abort(400)
